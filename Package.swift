7a0610b5bc4a2252001c016da322f26cf2e8b5f7
Denis Tretyakov <denis.tretyakov@sap.com>
2019-10-17 13:08:18 +0300

    Update package syntax

--- Package.swift
+++ Package.swift
@@ -9,17 +9,17 @@ let package = Package(
         .library(name: "HTTP", targets: ["RTMP"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/shogo4405/Logboard.git", from: "2.1.2")
+        .package(url: "https://github.com/shogo4405/Logboard.git", from: "2.1.2")
     ],
     targets: [
-        .target(name: "Codec", dependencies: [])
-        .target(name: "Extension", dependencied: [])
-        .target(name: "HTTP", dependencies: [])
-        .target(name: "ISO", dependencies: [])
-        .target(name: "Media", dependencies: [])
-        .target(name: "Util", dependencies: [])
-        .target(name: "Net", dependencies: ["Codec", "Extension", "ISO", "Media", "Util"])
-        .target(name: "HTTP", dependencies: ["Net"])
+        .target(name: "Codec"),
+        .target(name: "Extension"),
+        .target(name: "HTTP"),
+        .target(name: "ISO"),
+        .target(name: "Media"),
+        .target(name: "Util"),
+        .target(name: "Net", dependencies: ["Codec", "Extension", "ISO", "Media", "Util"]),
+        .target(name: "HTTP", dependencies: ["Net"]),
         .target(name: "RTMP", dependencies: ["Net", "FLV"])
     ]
 )
